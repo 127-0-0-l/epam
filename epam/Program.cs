a25a78a4d5474b31cd900835b20fe52944c58160
Maksim <borisovets.maksim@gmail.com>
2021-04-16 00:32:49 +0300

    Add project files.

---
+++ epam/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace epam
+{
+    class Program
+    {
+        static void Main()
+        {
+            FirstTask.Run();
+            SecondTask.Run();
+            ThirdTask.Run();
+        }
+    }
+}
---
+++ epam/ThirdTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace epam
+{
+    class ThirdTask
+    {
+        static Action<string, DateTime, int, int> TicketInfo;
+
+        public static void Run()
+        {
+            Console.WriteLine("\ntask 3:");
+
+            TicketInfo = DisplayInfo;
+
+            Random random = new Random();
+            TicketInfo?.Invoke("20th Century Limited", DateTime.Now, random.Next(1, 20), random.Next(1, 50));
+            TicketInfo?.Invoke("Trans-Siberian Express", DateTime.Now, random.Next(1, 20), random.Next(1, 50));
+            TicketInfo?.Invoke("Orient Express", DateTime.Now, random.Next(1, 20), random.Next(1, 50));
+        }
+
+        public static void DisplayInfo(string trainName, DateTime arriveTime, int coachNumber, int seatNumber)
+        {
+            Console.WriteLine(
+                $"ticket info:\n" +
+                $"train name: {trainName}\n" +
+                $"arrive time: {arriveTime}\n" +
+                $"coach number: {coachNumber}\n" +
+                $"seat number: {seatNumber}\n");
+        }
+    }
+}
